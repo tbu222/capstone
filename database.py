3dc173370696c926bfc9308aa3fceed5b65002d0
Thinh Bui <thinhbui2911@gmail.com>
2023-03-09 16:52:10 -0800

    second commit

--- database.py
+++ database.py
@@ -1,42 +1,48 @@
 import sqlite3
 
-connection =sqlite3.connect('database.db')
+#Open database
+conn = sqlite3.connect('database.db')
 
-connection.execute('''CREATE TABLE users (
-    userId INTERGER PRIMARY KEY,
-    password TEXT,
-    email TEXT,
-    firstName TEXT,
-    address1 TEXT,
-    address2 TEXT,
-    zipcode TEXT,
-    city TEXT,
-    state TEXT,
-    country TEXT,
-    phone TEXT
-)''')
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password TEXT,
+		email TEXT,
+		firstName TEXT,
+		lastName TEXT,
+		address1 TEXT,
+		address2 TEXT,
+		zipcode TEXT,
+		city TEXT,
+		state TEXT,
+		country TEXT, 
+		phone TEXT
+		)''')
 
-connection.execute('''CREATE TABLE products(
-    productId INTEGER PRIMARY KEY,
-    name TEXT,
-    price REAL,
-    description TEXT,
-    image TEXT,
-    quantity INTEGER,
-    categoryId INTEGER,
-    FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
-)''')
+conn.execute('''CREATE TABLE products
+		(productId INTEGER PRIMARY KEY,
+		name TEXT,
+		price REAL,
+		description TEXT,
+		image TEXT,
+		stock INTEGER,
+		categoryId INTEGER,
+		FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
+		)''')
 
-connection.execute('''CREATE TABLE cart(
-    userId INTEGER,
-    productId INTEGER,
-    FOREIGN KEY(userId) REFERENCES users(userId),
-    FOREIGN KEY(productId) REFERENCES products(productId)
-)''')
+conn.execute('''CREATE TABLE kart
+		(userId INTEGER,
+		productId INTEGER,
+		FOREIGN KEY(userId) REFERENCES users(userId),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
 
-connection.execute('''CREATE TABLE categories(
-    categoryId INTEGER PRIMARY KEY,
-    name Text
-)''')
+conn.execute('''CREATE TABLE categories
+		(categoryId INTEGER PRIMARY KEY,
+		name TEXT
+		)''')
 
-connection.close()
+
+
+conn.close()
+
